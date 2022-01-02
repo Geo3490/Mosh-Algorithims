542361dc48a8b9d00b3b63bc8d0e49f96d34797f
Geo3490 <geo40127@gmail.com>
2022-01-02 08:25:24 -0500

    terms updated

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,8 +1,7 @@
 package com.careerdevs;
 
-public class Main {
+public class Main{
+    public static void main(String[] args){
 
-    public static void main(String[] args) {
-	// write your code here
     }
 }
